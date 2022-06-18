a7fc471e761b390e9db9d3b3061c8cdc33d4cb1f
VIP <pnckdevapps@gmail.com>
2022-06-19 06:08:20 +0700

    sd

--- config.php
+++ config.php
@@ -15,10 +15,10 @@ $sql_db_user = "adminavfreex";
 // MySQL Database Password
 $sql_db_pass = "adminavfreex@123456789"; //adminavfreex@123456789
 // MySQL Database Name
-$sql_db_name = "avfreex";
+$sql_db_name = "avfree";
 
 // Site URL
-$site_url = "http://x.avfreex24.com"; // e.g (http://example.com)
+$site_url = "https://x.avfreex24.com"; // e.g (http://example.com)
 
 // Purchase code
 $purchase_code = "uvluxhb4x0nngagk"; // Your purchase code, don't give it to anyone.
---
+++ themes/avx/info.php
@@ -0,0 +1,13 @@
+<?php
+$themeName = 'AVX';
+
+$themeFolder = 'avx';
+
+$themeAuthor = 'AVFREEX24';
+
+$themeAuthorUrl = 'mailto:avfreex24@gmail.com';
+
+$themeVirsion = '1.0';
+
+$themeImg = $themeFolder . '/themeLogo.png';
+?>
